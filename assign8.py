8dc05edc8aa6d09143a62563ba72c494358f4014
Zayed8 <138813969+Zayed8@users.noreply.github.com>
2023-07-13 14:20:23 +0530

    Add files via upload

---
+++ assign8.py
@@ -0,0 +1,27 @@
+class A:
+    c = 0
+    _b = 0
+    __a = 0
+    def __init__(self,c,_b,__a):
+        self.c = c
+        self._b = _b
+        self.__a = __a
+    def display_A(self):
+        print("Inside of Class A!")
+        print("Value of A",self.__a)
+        print("Value of B",self._b)
+        print("Value of C",self.c)
+
+class B(A):
+    def display_B(self):
+        print("Inside of Class B!")
+        try:
+            print("Value of A",self.__a)
+        except Exception:
+            print("Private variable invalid accessing Error Raised!!")
+        print("Value of B",self._b)
+        print("Value of C",self.c)
+
+b = B(1,2,3)
+b.display_A()
+b.display_B()
